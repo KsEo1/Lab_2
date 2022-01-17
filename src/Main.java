cd9e0e8b5c71f89e6d9b13ee2c1f1203150b7754
Paweł Leszczyński <pawelleszczynskiprogrammer@gmail.com>
2022-01-17 13:45:19 +0100

    ćw 4

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,29 @@ public class Main {
         c) w trzeciej pętli return
         */
 
+        for (byte i = 100; i < 802; i++)
+        {
+            if (i == 200)
+                break;
+            System.out.println(i + "");
+
+        }
+
+        for ( short s = 5335; s < 5355; s++)
+        {
+            if (s == 5340)
+                continue;
+            System.out.println(s + "");
+        }
+        for ( long l = 802; l < 1400; l++) {
+            if (l == 950)
+                return;
+            System.out.println(l + "");
+        }
+
+
+
 
     }
 }
+
